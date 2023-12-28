5fea91ae860bd5c0caef783448e0bb0a88b33b6b
肉豆蔻 <1023876294@qq.com>
2023-12-28 17:28:32 +0800

    自媒体-素材列表查询

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
@@ -1,9 +1,11 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
 import com.heima.wemedia.service.WmMaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,4 +23,8 @@ public class WmMaterialController {
         return wmMaterialService.uploadPicture(multipartFile);
     }
 
+    @PostMapping("/list")
+    public ResponseResult findList(@RequestBody WmMaterialDto dto){
+        return wmMaterialService.findList(dto);
+    }
 }
